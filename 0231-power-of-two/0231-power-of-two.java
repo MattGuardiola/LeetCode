db0c61cd58b25036f3ef020185f6ea3068222b67
Matt Guardiola <118485669+MattGuardiola@users.noreply.github.com>
2023-08-03 11:47:18 -0500

    Time: 1 ms (98.94%), Space: 39.4 MB (93.90%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
         if (n <= 0) {
-            return false; // Negative numbers and 0 are not powers of two
+            return false; 
         }
 
         return (n & (n - 1)) == 0;
