131a91cb2fc5838b8c2aa93bee3937e26542aaf8
Matt Guardiola <118485669+MattGuardiola@users.noreply.github.com>
2023-08-03 11:47:11 -0500

    Time: 1 ms (98.94%), Space: 39.5 MB (77.90%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -4,8 +4,6 @@ class Solution {
             return false; // Negative numbers and 0 are not powers of two
         }
 
-        // If n is a power of two, it will have exactly one bit set to 1.
-        // Using bitwise AND with n-1, if the result is 0, n is a power of two.
         return (n & (n - 1)) == 0;
     }
 }
