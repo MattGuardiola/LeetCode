e36ca4a763d7bbc444379725c51d6b1aacfda010
Matt Guardiola <118485669+MattGuardiola@users.noreply.github.com>
2023-08-04 12:50:23 -0500

    Time: 1 ms (90.68%), Space: 39.3 MB (85.66%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
+
+        do {
+            slow = calculateSumOfSquares(slow);
+            fast = calculateSumOfSquares(calculateSumOfSquares(fast));
+        } while (slow != fast);
+
+        return slow == 1;
+    }
+    
+    
+    private static int calculateSumOfSquares(int num) {
+        int sum = 0;
+        while (num != 0) {
+            int digit = num % 10;
+            sum += digit * digit;
+            num /= 10;
+        }
+        return sum;
+    }
+}
