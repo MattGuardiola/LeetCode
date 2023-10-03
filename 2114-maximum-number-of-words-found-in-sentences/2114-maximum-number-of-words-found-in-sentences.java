d03ee20dea3529f49f98363fc25ca1875b379453
Matt Guardiola <118485669+MattGuardiola@users.noreply.github.com>
2023-10-02 20:51:56 -0500

    Time: 3 ms (89.67%), Space: 42.9 MB (43.44%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.java
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.java
@@ -3,13 +3,13 @@ class Solution {
           int maxWords = 0;
         
         for (String sentence : sentences) {
-            // Split the sentence into words using space as a delimiter
+
             String[] words = sentence.split(" ");
             
-            // Count the number of words in the current sentence
+
             int wordCount = words.length;
             
-            // Update maxWords if the current sentence has more words
+
             if (wordCount > maxWords) {
                 maxWords = wordCount;
             }
