d4bd12fc901366c5c766dbc797194e1e2d2cea88
Matt Guardiola <118485669+MattGuardiola@users.noreply.github.com>
2023-10-02 20:51:04 -0500

    Time: 1 ms (71.65%), Space: 41.6 MB (30.28%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.java
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.java
@@ -1,6 +1,6 @@
 class Solution {
     public int finalValueAfterOperations(String[] operations) {
-         int x = 0;  // Initial value of X
+         int x = 0; 
         for (String operation : operations) {
             if (operation.contains("++")) {
                 x++;
