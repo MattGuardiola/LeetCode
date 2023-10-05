674d8c1861f052f6b5d9883e4ccae0a54861f27e
Matt Guardiola <118485669+MattGuardiola@users.noreply.github.com>
2023-10-04 22:03:28 -0500

    Time: 1 ms (100.00%), Space: 43.5 MB (81.89%) - LeetHub

--- 2828-check-if-a-string-is-an-acronym-of-words/2828-check-if-a-string-is-an-acronym-of-words.java
+++ 2828-check-if-a-string-is-an-acronym-of-words/2828-check-if-a-string-is-an-acronym-of-words.java
@@ -2,8 +2,8 @@ class Solution {
     public boolean isAcronym(List<String> words, String s) {
         StringBuilder acronym = new StringBuilder();
         for (String word : words) {
-        if (!word.isEmpty()) { // Check if the word is not empty
-            acronym.append(word.charAt(0)); // Append the first character of the word to the acronym
+        if (!word.isEmpty()) { 
+            acronym.append(word.charAt(0)); 
         }
     }
     
