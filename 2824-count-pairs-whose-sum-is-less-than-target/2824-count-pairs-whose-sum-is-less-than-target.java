bcbd6c3894d74b1792fa660dc37ced5f736f6329
Matt Guardiola <118485669+MattGuardiola@users.noreply.github.com>
2023-10-12 22:43:10 -0500

    Time: 3 ms (28.70%), Space: 40.7 MB (96.92%) - LeetHub

--- 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.java
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.java
@@ -1,17 +1,16 @@
 class Solution {
     public int countPairs(List<Integer> nums, int target) {
- Collections.sort(nums); // Sort the list in ascending order.
+ Collections.sort(nums);
     int count = 0;
     int left = 0;
     int right = nums.size() - 1;
 
     while (left < right) {
         if (nums.get(left) + nums.get(right) < target) {
-            // If the sum is less than the target, then all pairs with nums.get(left) as the first element will also satisfy the condition.
             count += (right - left);
             left++;
         } else {
-            // If the sum is greater than or equal to the target, move the right pointer to the left.
+
             right--;
         }
     }
