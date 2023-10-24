64f9d70a9838635d76bd3ac1635742b375f6a443
Matt Guardiola <118485669+MattGuardiola@users.noreply.github.com>
2023-10-23 20:15:46 -0500

    Time: 1 ms (25.59%), Space: 41.2 MB (15.59%) - LeetHub

--- 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.java
+++ 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.java
@@ -6,12 +6,9 @@ class Solution {
     for (int i = 0; i < n; i++) {
         int currentIndex = index[i];
         
-
         for (int j = n - 1; j > currentIndex; j--) {
             target[j] = target[j - 1];
         }
-        
-
         target[currentIndex] = nums[i];
     }
     
