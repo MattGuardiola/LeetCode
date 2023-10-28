a1902c04fbb5a0a9fdbc04a6950271267f3e88bc
Matt Guardiola <118485669+MattGuardiola@users.noreply.github.com>
2023-10-27 19:18:09 -0500

    Time: 1 ms (55.62%), Space: 43.2 MB (95.09%) - LeetHub

---
+++ 0832-flipping-an-image/0832-flipping-an-image.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[][] flipAndInvertImage(int[][] image) {
+        int len=image.length;
+        int l=image[0].length;
+        for(int i=0;i<len;i++){
+            for(int j=0;j<l/2;j++){
+                int temp=image[i][j];
+                image[i][j]=image[i][l-1-j];
+                image[i][l-1-j]=temp;
+            }
+        }
+        for(int i=0;i<l;i++){
+            for(int j=0;j<l;j++){
+                if(image[i][j]==0){
+                    image[i][j]=1;
+                }
+                else if(image[i][j]==1){
+                    image[i][j]=0;
+                }
+            }
+        }
+        return image;
+    }
+}
